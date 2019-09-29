0663b577802e21caecdad62962849b14c1f9850d
yourname <zhangyuelun123@sina.com>
2019-09-29 15:01:49 +0800

    处理登录响应的res

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-123
+  <div class="login-wrap">
+
+<el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
+     <h2>用户登录</h2>
+  <el-form-item label="用户名">
+    <el-input v-model="formdata.username"></el-input>
+  </el-form-item>
+  <el-form-item label="密码">
+    <el-input v-model="formdata.password"></el-input>
+  </el-form-item>
+<el-button class="login-btn" type="success" @click="handleLogin()">登录</el-button>
+</el-form>
   </div>
 </template>
 
@@ -8,14 +18,26 @@
 export default {
   data () {
     return {
-
+      formdata:{
+        username:'',
+        password:''
+      }
     }
   },
   created () {
 
   },
   methods: {
-
+handleLogin(){
+  this.$http.post(`login`,this.formdata).then((res)=>{
+    const {data:{data,meta:{msg,status}}} = res
+    if(status===200){
+     this.$message.success(msg)
+    }else{
+     this.$message.error(msg)
+    }
+  })
+}
   },
   components: {
 
@@ -24,5 +46,20 @@ export default {
 </script>
 
 <style scoped >
-
+.login-wrap{
+  background-color: #324152;
+  height: 100%;
+  display: flex;
+justify-content: center;
+  align-items: center;
+}
+.login-form{
+  width: 400px;
+  border-radius: 6px;
+  background-color: #fff;
+  padding: 30px;
+}
+.login-btn{
+  width: 100%;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import elementUi from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/assets/css/base.css'
 
+import axios from 'axios'
+axios.defaults.baseURL="http://localhost:8888/api/private/v1/"
+Vue.prototype.$http = axios
+
+Vue.use(elementUi)
 Vue.config.productionTip = false
 
 new Vue({
